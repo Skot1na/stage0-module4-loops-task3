0a167350aef8fdd4198950e4e522cf7e3b1d906c
Kuanysh <amandos.kuanys@mail.ru>
2023-06-19 22:49:56 +0600

    M4-3

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
     public void printAlphabet() {
+        char alpha;
+        for (alpha = 'a'; alpha < 'z'; alpha++){
+            System.out.println(alpha);
+        }
     }
 }
