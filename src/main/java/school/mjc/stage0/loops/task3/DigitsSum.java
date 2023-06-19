cf70910bcffcd3a83bce59f47b53ed65463067c1
Kuanysh <amandos.kuanys@mail.ru>
2023-06-20 01:54:09 +0600

    M4-3

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -1,7 +1,15 @@
 package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
-    public void printDigitsSum(int t){
+    public void printDigitsSum(int number) {
+        int sum = 0;
+        String numberString = number + "";
 
+        for (int i = 0; i < numberString.length(); i++) {
+            char digitChar = numberString.charAt(i);
+            int digit = digitChar - '0';
+            sum += digit;
+
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
+        int num;
+        int sum = 0;
+        int number = 9;
+        for (num = 9; num <= lengthOfLastNumber; num++){
+            sum += number;
+            number = number * 10 + 9;
+        }
+        System.out.println(sum);
 
     }
 }
