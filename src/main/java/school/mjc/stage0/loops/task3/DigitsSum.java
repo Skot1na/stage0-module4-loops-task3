c42490857b47e9f257f2e664a4f7248ceaebcae3
Kuanysh <amandos.kuanys@mail.ru>
2023-06-20 02:17:48 +0600

    M4-3

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,12 +2,11 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int number) {
-        int sum = 0;
-        for (int i = 0; i <= number; i++){
-            int digit = number%10;
-            sum += digit;
-            number = number / 10;
-            System.out.println(sum);
+        long sum;
+        for(sum=0; number!=0; number=number/10)
+        {
+            sum = sum + number % 10;
         }
+        System.out.println(sum);
     }
 }
