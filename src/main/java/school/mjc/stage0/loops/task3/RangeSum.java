0a0861178e13320e3f10062558e9c464bc2fcaef
Kuanysh <amandos.kuanys@mail.ru>
2023-06-19 22:45:43 +0600

    M4-3

--- src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.loops.task3;
 
 public class RangeSum {
     public void printSumInclusive(int firstBoarder, int secondBoarder) {
+        for (firstBoarder = 0; firstBoarder <= secondBoarder; firstBoarder++){
+            System.out.println(firstBoarder);
+        }
     }
 }
