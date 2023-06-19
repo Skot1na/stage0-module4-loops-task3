838dbe12748ee71d7ffd5deaaf6b84bddbf669fe
Kuanysh <amandos.kuanys@mail.ru>
2023-06-20 01:57:43 +0600

    M4-3

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -2,8 +2,7 @@ package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
     public void printAlphabet() {
-        char alpha;
-        for (alpha = 'a'; alpha < 'z'; alpha++){
+        for (char alpha = 'a'; alpha < 'z'; alpha++){
             System.out.println(alpha);
         }
     }
--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        int firstTerm = 0;
+        int secondTerm = 1;
+
+        System.out.println("Fibonacci Series:");
+
+        for (int i = 1; i <= lastFibonacci; i++) {
+            System.out.println(firstTerm);
+
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
     }
 }
